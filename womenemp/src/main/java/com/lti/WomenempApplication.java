22848b4d56a41c2c5a1b4f5d0b137b144246ae63
Ajay Choudhari <ajaychoudhari35@gmail.com>
2022-08-01 14:54:48 +0530

    Push

--- womenemp/src/main/java/com/lti/WomenempApplication.java
+++ womenemp/src/main/java/com/lti/WomenempApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WomenempApplication {
 
-	//AjayGit - token  : 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
+	//AjayGit - token  : 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
 	
 	public static void main(String[] args) {
 		SpringApplication.run(WomenempApplication.class, args);
