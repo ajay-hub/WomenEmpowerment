13912d27b2e11b93426cab4338c2e3d979927517
Ajay Choudhari <ajaychoudhari35@gmail.com>
2022-08-02 23:32:50 +0530

    Swoosh

--- womenemp/src/main/java/com/lti/WomenempApplication.java
+++ womenemp/src/main/java/com/lti/WomenempApplication.java
@@ -7,6 +7,7 @@ public class WomenempApplication {
 
 	//AjayGit - token  : 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
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WomenempApplication.class, args);
 	}
